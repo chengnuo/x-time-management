99138cdd36bc27a2bd5d07310b4d8f21f4cefc96
xjc <2585441871@qq.com>
2017-07-17 18:26:55 +0800

    restful

--- app/controller/posts.js
+++ app/controller/posts.js
@@ -2,9 +2,18 @@ exports.index = function* (ctx) {
     console.log("index")
     ctx.body = "index";
 };
-exports.new = function* () {};
-exports.create = function* () {};
-exports.show = function* () {};
+exports.new = function* (ctx) {
+    console.log("new")
+    ctx.body = "new";
+};
+exports.create = function* (ctx) {
+    console.log("create")
+    ctx.body = "create";
+};
+exports.show = function* (ctx) {
+    console.log("show")
+    ctx.body = "show";
+};
 exports.edit = function* () {};
 exports.update = function* () {};
 exports.destroy = function* () {};
