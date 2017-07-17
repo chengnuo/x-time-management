4c36920f9a0e2f6ea430d1a6d3f34d6ec86980d3
xjc <2585441871@qq.com>
2017-07-17 09:30:56 +0800

    egg

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1500254867845_5769';
+
+  // add your config here
+
+  return config;
+};
